f7310491802710a9ed252fa9fe1077ae43b7b0fe
Andrey Ermoshin <iermoshin@gmail.com>
2017-10-31 12:20:21 +0300

    Some comments and search method for ref type

--- BinaryTree.playground/Contents.swift
+++ BinaryTree.playground/Contents.swift
@@ -6,18 +6,34 @@ import Foundation
 // https://dispatchswift.com/generic-protocols-in-swift-b47414e29bba
 // https://milen.me/writings/swift-generic-protocols/
 
-protocol Insertable{
+// http://cslibrary.stanford.edu/110/BinaryTrees.html
+// Basically, binary search trees are fast at insert and lookup.
+
+
+protocol Treelike {
     associatedtype Element: Comparable
     func insert(valueForInsertion: Element) -> Self
+    func search(for value: Element) -> Self?
 }
 
-class BinaryTreeNodeRefType<T: Comparable> {
+// Use 'final' keyword for class to meet requirements from compiler
+// for returning Self from function
+// specifically for search
+final class BinaryTreeNodeRefType<T: Comparable> {
     private let value: T
     // parent can be nil if it is root node
     private weak var parent: BinaryTreeNodeRefType?
     // childs should be stored by strong references
     private var left: BinaryTreeNodeRefType?
     private var right: BinaryTreeNodeRefType?
+    
+    // to protect from empty case
+    // private init()
+    // even if that init method is never used
+    // compiler wants to initialize non optional values
+    // but it is not possible or not convinient
+    // value = T.init()
+    // because for example Int can't be emtpy, but for example String could be
     
     init(newValue: T) {
         value = newValue
@@ -29,11 +45,12 @@ class BinaryTreeNodeRefType<T: Comparable> {
 // So, no need to write something like next
 // extension BinaryTreeNodeRefType<T: Comparable>: Insertable
 
-extension BinaryTreeNodeRefType: Insertable {
+extension BinaryTreeNodeRefType: Treelike {
     typealias Element = T
+
     public func insert(valueForInsertion: Element) -> Self {
         // to maintain balanced binary tree
-        // need to insert first from root node
+        // need to search place where to insert firstly from the root node
         if valueForInsertion < value {
             if let left = left {
                 left.insert(valueForInsertion: valueForInsertion)
@@ -56,7 +73,42 @@ extension BinaryTreeNodeRefType: Insertable {
         // self but it actually will not be used
         return self
     }
+    
+    func search(for value: T) -> BinaryTreeNodeRefType? {
+        // 'Self' is only available in a protocol or as the result of a method in a class
+        var result: BinaryTreeNodeRefType?
+        // empty case for class implementation is not justified
+        // because if describe the value as Optional
+        // then it will be very bad for performance to always cast from Optional to real type
+        // during search or insert
+        
+        if value == self.value {
+            return self
+        }
+        else {
+            if let left = left {
+                if value == left.value {
+                    return left
+                }
+                else if value < left.value {
+                    return left.search(for: value)
+                }
+            }
+            if let right = right {
+                if value == right.value {
+                    return right
+                }
+                else if value > right.value {
+                    return right.search(for: value)
+                }
+            }
+        }
+        
+        return result
+    }
 }
+
+// Note: tree implementation using
 
 enum BinaryTreeNodeEnum<T: Comparable> {
     case empty
@@ -72,7 +124,7 @@ enum BinaryTreeNodeEnum<T: Comparable> {
     }
 }
 
-extension BinaryTreeNodeEnum: Insertable {
+extension BinaryTreeNodeEnum: Treelike {
     typealias Element = T
     public func insert(valueForInsertion: Element) -> BinaryTreeNodeEnum {
 
@@ -95,6 +147,10 @@ extension BinaryTreeNodeEnum: Insertable {
             }
         }
     }
+    
+    func search(for value: T) -> BinaryTreeNodeEnum<T>? {
+        return nil
+    }
 }
 
 extension BinaryTreeNodeEnum: CustomStringConvertible {
@@ -116,4 +172,6 @@ var enumTree = BinaryTreeNodeEnum<Int>(newValue: 0)
 enumTree = enumTree.insert(valueForInsertion: -1)
 enumTree = enumTree.insert(valueForInsertion: 2)
 enumTree = enumTree.insert(valueForInsertion: 4)
+enumTree = enumTree.insert(valueForInsertion: -2)
+enumTree = enumTree.insert(valueForInsertion: 1)
 print(enumTree.description)
