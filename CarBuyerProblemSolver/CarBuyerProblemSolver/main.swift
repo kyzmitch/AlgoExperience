c50aae6bd5ecbb05382f8e033cafc75cc43e3559
Andrei Ermoshin <iermoshin@gmail.com>
2017-11-08 21:49:40 +0300

    New console app to solve problem

---
+++ CarBuyerProblemSolver/CarBuyerProblemSolver/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  CarBuyerProblemSolver
+//
+//  Created by admin on 08/11/2017.
+//  Copyright © 2017 kyzmitch. All rights reserved.
+//
+
+import Foundation
+
+// https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=608&page=show_problem&problem=1055
+
+
