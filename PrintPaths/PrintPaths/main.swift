38de00119cf87708cfde2c57cf4eb07994171207
Andrey Ermoshin <iermoshin@gmail.com>
2017-11-13 17:32:51 +0300

    Working print paths

--- PrintPaths/PrintPaths/main.swift
+++ PrintPaths/PrintPaths/main.swift
@@ -49,34 +49,22 @@ class BinaryTreeNodeRefType {
     
     private func valuePathFromRoot(previousPaths: [[Int]]) -> [[Int]] {
         if isLeaf() {
-            var pathIndex: Int?
-            for i in 0..<previousPaths.count {
-                let path = previousPaths[i]
-                if let lastValue = path.last {
-                    if lastValue == value {
-                        pathIndex = i
-                        break
-                    }
-                }
-            }
-            
-            if let pathIndex = pathIndex {
-                var leafPath = previousPaths[pathIndex]
-                leafPath.append(value)
-                return [leafPath]
-            }
-            else {
-                return [[Int]]()
+            if let parent = parent {
+                let result = previousPaths.getAppendedSubArray(with: parent.value, newValue: value)
+                return result != nil ? result! : [[Int]]()
             }
         }
         else {
+            var input = previousPaths
+            input.appendSubarray(with: parent!.value, newValue: value)
+            
             var leftPaths: [[Int]]?
             if let left = left {
-                leftPaths = left.valuePathFromRoot(previousPaths: previousPaths)
+                leftPaths = left.valuePathFromRoot(previousPaths: input)
             }
             var rightPaths: [[Int]]?
             if let right = right {
-                rightPaths = right.valuePathFromRoot(previousPaths: previousPaths)
+                rightPaths = right.valuePathFromRoot(previousPaths: input)
             }
             if leftPaths != nil && rightPaths != nil {
                 var r = leftPaths!
@@ -90,16 +78,16 @@ class BinaryTreeNodeRefType {
                 return rightPaths!
             }
         }
+        return [[Int]]()
     }
     
-    func printPaths() {
+    func printPaths() -> Void {
         // Given a binary tree, print out all of its root-to-leaf paths, one per line.
         if parent == nil && isLeaf() {
             print("Empty")
-            return
         }
         else {
-            let rootPart = Array<Array<Int>>([value])
+            let rootPart = [[value]]
             if let left = left {
                 let leftPaths = left.valuePathFromRoot(previousPaths: rootPart)
                 print("\(leftPaths)")
@@ -112,12 +100,52 @@ class BinaryTreeNodeRefType {
     }
 }
 
+extension Array where Element == Array<Int> {
+    nonmutating func getAppendedSubArray(with endValue: Int, newValue: Int) -> [[Int]]? {
+        var pathIndex: Int?
+        for (index, element) in self.enumerated() {
+            if let lastValue = element.last {
+                if lastValue == endValue {
+                    pathIndex = index
+                    break
+                }
+            }
+        }
+        
+        if let pathIndex = pathIndex {
+            var leafPath = self[pathIndex]
+            leafPath.append(newValue)
+            return [leafPath]
+        }
+        return nil
+    }
+    
+    mutating func appendSubarray(with endValue: Int, newValue: Int) {
+        var pathIndex: Int?
+        for (index, element) in self.enumerated() {
+            if let lastValue = element.last {
+                if lastValue == endValue {
+                    pathIndex = index
+                    break
+                }
+            }
+        }
+        
+        if let pathIndex = pathIndex {
+            var leafPath = self[pathIndex]
+            leafPath.append(newValue)
+            self[pathIndex] = leafPath
+        }
+    }
+}
+
 let tree = BinaryTreeNodeRefType(newValue: 0)
 tree.insert(valueForInsertion: -1)
 tree.insert(valueForInsertion: 2)
 tree.insert(valueForInsertion: -2)
 tree.insert(valueForInsertion: 3)
 tree.insert(valueForInsertion: 4)
+tree.insert(valueForInsertion: 1)
 tree.printPaths()
 
 
