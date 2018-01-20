98bdddfe82d0e3fc56e961e9070e36cf4feb2720
Andrey Ermoshin <iermoshin@gmail.com>
2018-01-21 01:47:10 +0300

    String - reverse - recursively

---
+++ Strings/StringReverse/StringReverse/main.swift
@@ -0,0 +1,46 @@
+//
+//  main.swift
+//  StringReverse
+//
+//  Created by Andrey Ermoshin on 21/01/2018.
+//  Copyright © 2018 andreiermoshin. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    mutating func recursiveReverse() -> Void {
+        if count < 2 {
+            return
+        }
+        let middleLength = self.count / 2
+        // NOTE: note efficient way to recurse String
+        // it is need to create copy by using Array
+        // so, it seems there is no possibility
+        // to do in-place recursion/mutating
+        // because subscript on String is get only
+        var tempArray: Array<String.Element> = Array(self)
+        internalReverse(0, middleLength, &tempArray)
+    }
+    
+    private mutating func internalReverse(_ i: Int,_ middleIndex: Int,_ array: inout Array<String.Element>) {
+        
+        let temp = array[i]
+        let distance = middleIndex - i
+        let j = i + 2 * distance
+        array[i] = array[j]
+        array[j] = temp
+        let nextIndex = i + 1
+        if nextIndex == middleIndex {
+            self = String(array)
+            return
+        }
+        else {
+            internalReverse(nextIndex, middleIndex, &array)
+        }
+    }
+}
+
+var s = "abcde"
+s.recursiveReverse()
+print("Reverse: \(s)")
