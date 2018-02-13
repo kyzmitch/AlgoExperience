4c75dbb48e22fb49f2ea91dd8713308ceff6585c
Andrey Ermoshin <iermoshin@gmail.com>
2018-02-13 10:29:05 +0300

    Linked List - delete node

---
+++ DataStructures/LinkedList/Delete Node in a Linked List/Delete Node in a Linked List/main.cpp
@@ -0,0 +1,55 @@
+//
+//  main.cpp
+//  Delete Node in a Linked List
+//
+//  Created by Andrey Ermoshin on 13/02/2018.
+//  Copyright © 2018 andreiermoshin. All rights reserved.
+//
+
+#include <iostream>
+
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        if (node == nullptr) {
+            return;
+        }
+        // NOTE: can't remove if node is the last node
+        if (node->next == nullptr) {
+            return;
+        }
+        ListNode *i = node->next;
+        ListNode *previous = node;
+        while (true) {
+            previous->val = i->val;
+            if (i->next != nullptr) {
+                previous = i;
+                i = i->next;
+            }
+            else {
+                break;
+            }
+        }
+        previous->next = nullptr;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    Solution solver;
+    ListNode *r1 = new ListNode(1);
+    r1->next = new ListNode(2);
+    r1->next->next = new ListNode(3);
+    r1->next->next->next = new ListNode(4);
+    ListNode *nodeToRemove = r1->next->next;
+    
+    solver.deleteNode(nodeToRemove);
+    std::cout << "After removing\n";
+    return 0;
+}
