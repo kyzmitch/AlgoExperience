65c540e9ad15e4603934cb5d980af30362c6418a
Andrey Ermoshin <iermoshin@gmail.com>
2018-02-15 19:13:42 +0300

    Math - perfect squares (DP solution)

--- Math/Perfect Squares/Perfect Squares/main.swift
+++ Math/Perfect Squares/Perfect Squares/main.swift
@@ -15,19 +15,16 @@ import Foundation
 // given n = 13, return 2 because 13 = 4 + 9.
 
 class Solution {
-    func numSquares(_ n: Int) -> Int {
+    func numSquaresWithSlowDictionaryCache(_ n: Int) -> Int {
         var cache = Dictionary<Int, Int>()
         cache[1] = 1
         cache[2] = 2
         cache[3] = 3
         cache[4] = 1
-        return numSquaresHelper(n, &cache)
+        return numSquaresHelperForDictionary(n, &cache)
     }
     
-    func numSquaresHelper(_ n: Int, _ cache: inout Dictionary<Int, Int>) -> Int {
-        if let cachedValue = cache[n] {
-            return cachedValue
-        }
+    func numSquaresHelperForDictionary(_ n: Int, _ cache: inout Dictionary<Int, Int>) -> Int {
         
         var num = Int.max
         var currentNum = 0
@@ -42,8 +39,12 @@ class Solution {
                 cache[n] = 1
                 return 1
             }
-            
-            currentNum += numSquaresHelper(secondRemain, &cache)
+            if let cachedValue = cache[n] {
+                 currentNum += cachedValue
+            }
+            else {
+                currentNum += numSquaresHelperForDictionary(secondRemain, &cache)
+            }
             num = min(num, currentNum)
             currentNum = 0
             lowerBound -= 1
@@ -51,11 +52,102 @@ class Solution {
         cache[n] = num
         return num
     }
+    
+    func numSquaresWithArrayCache(_ n: Int) -> Int {
+        var cache = Array<Int>(repeatElement(0, count: n + 1))
+        return numSquaresHelperForArray(n, &cache)
+    }
+    
+    func numSquaresHelperForArray(_ n: Int, _ cache: inout Array<Int>) -> Int {
+        
+        // Bottom-up DP solution
+        var num = Int.max
+        var currentNum = 0
+        
+        let root = sqrt(Double(n))
+        var lowerBound = Int(floor(root))
+        
+        while lowerBound != 0 {
+            let secondRemain = n - lowerBound * lowerBound
+            currentNum += 1
+            if secondRemain == 0 {
+                cache[n] = 1
+                return 1
+            }
+            let cachedValue = cache[n]
+            if cachedValue > 0 {
+                currentNum += cachedValue
+            }
+            else {
+                currentNum += numSquaresHelperForArray(secondRemain, &cache)
+            }
+            num = min(num, currentNum)
+            currentNum = 0
+            lowerBound -= 1
+        }
+        cache[n] = num
+        return num
+    }
+    
+    func numSquaresWithDP(_ n: Int) -> Int {
+        // DP solution from leetcode
+        if n <= 0 {
+            return 0
+        }
+        
+        var cntPerfectSquares = Array(repeatElement(Int.max, count: n + 1))
+        cntPerfectSquares[0] = 0
+        for i in (1...n) {
+            // For each i, it must be the sum of some number (i - j*j) and
+            // a perfect square number (j*j).
+            var j = 1
+            var squredJ = 1
+            while squredJ <= i {
+                cntPerfectSquares[i] = min(cntPerfectSquares[i], cntPerfectSquares[i - squredJ] + 1)
+                j += 1
+                squredJ = j * j
+            }
+        }
+        
+        return cntPerfectSquares[n]
+    }
+    
+    func numSquares(_ n: Int) -> Int {
+        // Static Dynamic Programming from leetcode
+        if n <= 0 {
+            return 0
+        }
+        
+        // cntPerfectSquares[i] = the least number of perfect square numbers
+        // which sum to i. Since cntPerfectSquares is a static vector, if
+        // cntPerfectSquares.size() > n, we have already calculated the result
+        // during previous function calls and we can just return the result now.
+        var cntPerfectSquares = Array<Int>()
+        cntPerfectSquares.append(0)
+        // While cntPerfectSquares.size() <= n, we need to incrementally
+        // calculate the next result until we get the result for n.
+        while cntPerfectSquares.count <= n {
+            let m = cntPerfectSquares.count
+            var cntSquares = Int.max;
+            var i = 1
+            var squaredI = 1
+            
+            while squaredI <= m {
+                cntSquares = min(cntSquares, cntPerfectSquares[m - squaredI] + 1)
+                i += 1
+                squaredI = i * i
+            }
+            
+            cntPerfectSquares.append(cntSquares)
+        }
+        
+        return cntPerfectSquares[n]
+    }
 }
 
 let solver =  Solution()
 
-print("Minimum number of perfect square numbers for \(47) is \(solver.numSquares(47))") // ?
-print("Minimum number of perfect square numbers for \(48) is \(solver.numSquares(48))") // 3  according to leetcode
+print("Minimum number of perfect square numbers for \(47) is \(solver.numSquares(47))") // 4
+print("Minimum number of perfect square numbers for \(48) is \(solver.numSquares(48))") // 3
 print("Minimum number of perfect square numbers for \(12) is \(solver.numSquares(12))") // 3
 print("Minimum number of perfect square numbers for \(13) is \(solver.numSquares(13))") // 2
