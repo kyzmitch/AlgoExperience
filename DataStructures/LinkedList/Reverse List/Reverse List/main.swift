0a9d6bc5647e994b8cb3e00146ec24a2caf7fdef
Andrei Ermoshin <iermoshin@gmail.com>
2018-02-21 16:54:33 +0300

    Linked List - reverse (in place) - comments

--- DataStructures/LinkedList/Reverse List/Reverse List/main.swift
+++ DataStructures/LinkedList/Reverse List/Reverse List/main.swift
@@ -20,15 +20,24 @@ class Node {
 let head = Node(1, Node(2, Node(3, nil)))
 
 func reverse(_ head: Node) -> Node {
-    var previous: Node?
     var current: Node? = head
+    var previous: Node?
     var next: Node?
+    
     while current != nil {
+        // save reference to next element
         next = current?.next
+        // swap nodes or add one more node to reversed node
+        // now current is new head of reversed node
         current?.next = previous
+        // move previous on next position
+        // which was remembered at the end of previous iteration
         previous = current
+        // move current to next position
+        // which was remembered at the beginning of current iteration
         current = next
     }
+    
     return previous!
 }
 
