dae910ebd090d6425db17ab66714e66aacd76d66
Andrey Ermoshin <iermoshin@gmail.com>
2018-03-02 23:20:06 +0300

    Binary Tree - AVL - insert (refactor)

--- DataStructures/BinaryTrees/AVLTree/AVLTree/AVLTree.swift
+++ DataStructures/BinaryTrees/AVLTree/AVLTree/AVLTree.swift
@@ -25,14 +25,6 @@ class BTNode<T: Comparable> {
         left = l
         right = r
     }
-}
-
-class BSTTree<T: Comparable> {
-    typealias TreePayload = T
-    var head: BTNode<T>?
-    init(_ h: BTNode<T>) {
-        head = h
-    }
     
     class func height(_ node: BTNode<T>?) -> Int {
         if let n = node {
@@ -41,11 +33,75 @@ class BSTTree<T: Comparable> {
         return 0
     }
     
-    class func balance(_ node: BTNode<T>?) -> Int {
-        if let n = node {
-            return BSTTree.height(n.left) - BSTTree.height(n.right)
-        }
-        return 0
+    class func balance(_ node: BTNode<T>) -> Int {
+        return BTNode.height(node.left) - BTNode.height(node.right)
+    }
+    
+    func rotatedRight() -> BTNode<T> {
+        /*
+         T1, T2, T3 and T4 are subtrees.
+                z                                      y
+               / \                                   /   \
+              y   T4      Right Rotate (z)          x      z
+             / \          - - - - - - - - ->      /  \    /  \
+            x   T3                               T1  T2  T3  T4
+           / \
+         T1  T2
+         
+         */
+        
+        let y = self
+        let x = y.left!
+        let T2 = x.right
+        
+        // Perform rotation
+        x.right = y
+        y.left = T2
+        
+        // Update heights
+        y.height = max(BTNode.height(y.left), BTNode.height(y.right)) + 1;
+        x.height = max(BTNode.height(x.left), BTNode.height(x.right)) + 1;
+        
+        // Return new root
+        return x
+    }
+    
+    func rotatedLeft() -> BTNode<T> {
+        /*
+         
+         z                               y
+        / \                            /   \
+          T1   y     Left Rotate(z)   z      x
+         /  \   - - - - - - - ->    / \    / \
+        T2   x                     T1  T2 T3  T4
+            / \
+          T3  T4
+         
+         */
+
+        
+        let x = self
+        let y = x.right!
+        let T2 = y.left
+        
+        // Perform rotation
+        y.left = x
+        x.right = T2
+        
+        //  Update heights
+        x.height = max(BTNode.height(x.left), BTNode.height(x.right)) + 1
+        y.height = max(BTNode.height(y.left), BTNode.height(y.right)) + 1
+        
+        // Return new root
+        return y
+    }
+}
+
+class BSTTree<T: Comparable> {
+    typealias TreePayload = T
+    var head: BTNode<T>?
+    init(_ h: BTNode<T>) {
+        head = h
     }
     
     func preorderPrint() {
@@ -90,20 +146,14 @@ class BSTTree<T: Comparable> {
         }
         
         // update height
-        node.height = 1 + max(BSTTree.height(node.left), BSTTree.height(node.right))
+        node.height = 1 + max(BTNode.height(node.left), BTNode.height(node.right))
         
         // unchanged
         return node
     }
 }
 
-protocol TreeRotating {
-    associatedtype TreePayload: Comparable
-    static func rotateRight(_ y: BTNode<TreePayload>) -> BTNode<TreePayload>
-    static func rotateLeft(_ x: BTNode<TreePayload>) -> BTNode<TreePayload>
-}
-
-class AVLTree<T: Comparable>: BSTTree<T>, TreeRotating {
+class AVLTree<T: Comparable>: BSTTree<T> {
     
     override fileprivate func insert(_ value: T, _ node: BTNode<T>?) -> BTNode<T> {
         // Perform standard BST insert for value
@@ -124,92 +174,41 @@ class AVLTree<T: Comparable>: BSTTree<T>, TreeRotating {
         }
         
         // update height
-        node.height = 1 + max(BSTTree.height(node.left), BSTTree.height(node.right))
+        node.height = 1 + max(BTNode.height(node.left), BTNode.height(node.right))
         
         // If this node becomes unbalanced, then
         // there are 4 cases
-        let balance = AVLTree.balance(node)
+        let balance = BTNode.balance(node)
         
-        // Left Left Case
-        if balance > 1 && value < node.left!.value {
-            return AVLTree.rotateRight(node)
+        if balance > 1 {
+            if let l = node.left {
+                // Left Left Case
+                if value < l.value {
+                    return node.rotatedRight()
+                }
+                // Left Right Case
+                if value > l.value {
+                    node.left = l.rotatedLeft()
+                    return node.rotatedRight()
+                }
+            }
+            
+        }
+        else if balance < -1 {
+            if let r = node.right {
+                // Right Right Case
+                if value > r.value {
+                    return node.rotatedLeft()
+                }
+                // Right Left Case
+                if value < r.value {
+                    node.right = r.rotatedRight()
+                    return node.rotatedLeft()
+                }
+            }
         }
         
-        // Right Right Case
-        if balance < -1 && value > node.right!.value {
-            return AVLTree.rotateLeft(node)
-        }
-        
-        // Left Right Case
-        if balance > 1 && value > node.left!.value {
-            node.left = AVLTree.rotateLeft(node.left!);
-            return AVLTree.rotateRight(node)
-        }
-        
-        // Right Left Case
-        if balance < -1 && value < node.right!.value {
-            node.right = AVLTree.rotateRight(node.right!)
-            return AVLTree.rotateLeft(node)
-        }
-        
-        // return the (unchanged) node pointer
+        // return the (unchanged) node
         return node
-    }
-    
-    static func rotateRight(_ y: BTNode<TreePayload>) -> BTNode<TreePayload> {
-        /*
-         T1, T2, T3 and T4 are subtrees.
-               z                                      y
-              / \                                   /   \
-             y   T4      Right Rotate (z)          x      z
-            / \          - - - - - - - - ->      /  \    /  \
-           x   T3                               T1  T2  T3  T4
-          / \
-         T1  T2
-         
-         */
-        
-        
-        let x = y.left!
-        let T2 = x.right
-        
-        // Perform rotation
-        x.right = y
-        y.left = T2
-        
-        // Update heights
-        y.height = max(AVLTree.height(y.left), AVLTree.height(y.right)) + 1;
-        x.height = max(AVLTree.height(x.left), AVLTree.height(x.right)) + 1;
-        
-        // Return new root
-        return x
-    }
-    
-    static func rotateLeft(_ x: BTNode<TreePayload>) -> BTNode<TreePayload> {
-        /*
-         
-       z                                y
-      / \                            /   \
-    T1   y     Left Rotate(z)       z      x
-        /  \   - - - - - - - ->    / \    / \
-       T2   x                     T1  T2 T3  T4
-           / \
-         T3  T4
-         
-         */
-        
-        let y = x.right!
-        let T2 = y.left
-        
-        // Perform rotation
-        y.left = x
-        x.right = T2
-        
-        //  Update heights
-        x.height = max(AVLTree.height(x.left), AVLTree.height(x.right)) + 1
-        y.height = max(AVLTree.height(y.left), AVLTree.height(y.right)) + 1
-        
-        // Return new root
-        return y
     }
 }
