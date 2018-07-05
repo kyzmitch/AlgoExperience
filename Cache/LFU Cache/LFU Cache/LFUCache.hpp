121e471422a97351e3d86720c4eeb159137a4cbc
Andrei Ermoshin <iermoshin@gmail.com>
2018-07-05 20:10:14 +0300

    Cache - LFU (put - not finished)

--- Cache/LFU Cache/LFU Cache/LFUCache.hpp
+++ Cache/LFU Cache/LFU Cache/LFUCache.hpp
@@ -23,6 +23,31 @@ private:
     std::vector<std::list<ListPair>> frequencies;
     int maxSize;
     const unsigned char listAmount = 4;
+    
+    void moveToAnotherFrequency(unsigned char index,
+                                std::list<ListPair> list,
+                                std::list<ListPair>::iterator iterator) {
+        // need to move to next list to the end of it
+        if (index < listAmount - 1) {
+            auto nextList = frequencies[index + 1];
+            // Need to move element from the top of
+            // one list to the bottom of another list
+            nextList.splice(nextList.end(), list, iterator);
+            // https://stackoverflow.com/a/4912492
+        }
+        // else we can't move element, it is on the top already
+    }
+    
+    void moveInSameFrequency(std::list<ListPair> list,
+                             std::list<ListPair>::iterator iterator) {
+        // if it's not on the top of the list
+        // then need to move element just one position ahead
+        auto nextIterator = ++iterator;
+        // single element (2) splice method should have 3 arguments
+        // http://www.cplusplus.com/reference/list/list/splice/
+        list.splice(nextIterator, list, iterator);
+    }
+    
 public:
     LFUCache(int capacity): maxSize(capacity) {
         for (int i = 0; i < listAmount; i++) {
@@ -41,22 +66,9 @@ public:
         auto list = frequencies[index];
         auto iterator = node->second.second;
         if (iterator == list.begin()) {
-            // need to move to next list to the end of it
-            if (index < listAmount - 1) {
-                auto nextList = frequencies[index + 1];
-                // Need to move element from the top of
-                // one list to the bottom of another list
-                nextList.splice(nextList.end(), list, iterator);
-                // https://stackoverflow.com/a/4912492
-            }
-            // else we can't move element, it is on the top already
+            moveToAnotherFrequency(index, list, iterator);
         } else {
-            // if it's not on the top of the list
-            // then need to move element just one position ahead
-            auto nextIterator = ++iterator;
-            // single element (2) splice method should have 3 arguments
-            // http://www.cplusplus.com/reference/list/list/splice/
-            list.splice(nextIterator, list, iterator);
+            moveInSameFrequency(list, iterator);
         }
         
         int value = node->second.second->second;
@@ -64,7 +76,43 @@ public:
     }
     
     void put(int key, int value) {
+        auto node = hashMap.find(key);
+        if (node != hashMap.end()) {
+            // need update value and move element up in the queue
+            node->second.second->second = value;
+            unsigned char index = node->second.first;
+            auto iterator = node->second.second;
+            auto list = frequencies[index];
+            if (iterator == list.begin()) {
+                moveToAnotherFrequency(index, list, iterator);
+            } else {
+                moveInSameFrequency(list, iterator);
+            }
+            return;
+        }
         
+        if (hashMap.size() == maxSize) {
+            // need to remove last value from the last
+            // non empty list
+            for (int i = 0; i < listAmount; i++) {
+                auto list = frequencies[i];
+                if (list.size() != 0) {
+                    auto lastNode = list.back();
+                    hashMap.erase(lastNode.first);
+                    list.pop_back();
+                    break;
+                }
+            }
+        }
+        
+        // Now need to insert completely new value to cache
+        ListPair pair(key, value);
+        auto firstList = frequencies.front();
+        firstList.push_back(pair);
+        auto insertedIterator = firstList.end()--;
+        HashMapPair subPair(0, insertedIterator);
+        std::pair<KeyType, HashMapPair> cachePair(key, subPair);
+        hashMap.insert(cachePair);
     }
 };
 
--- Cache/LFU Cache/LFU Cache/main.cpp
+++ Cache/LFU Cache/LFU Cache/main.cpp
@@ -10,7 +10,19 @@
 #include "LFUCache.hpp"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    LFUCache cache(2 /* capacity */);
+    
+    cache.put(1, 1);
+    cache.put(2, 2);
+    std::cout << cache.get(1) << std::endl; // returns 1
+    cache.put(3, 3);    // evicts key 2
+    
+    std::cout << cache.get(2) << std::endl; // returns -1 (not found)
+    std::cout << cache.get(3) << std::endl; // returns 3.
+    cache.put(4, 4);    // evicts key 1.
+    
+    std::cout << cache.get(1) << std::endl; // returns -1 (not found)
+    std::cout << cache.get(3) << std::endl; // returns 3
+    std::cout << cache.get(4) << std::endl; // returns 4
     return 0;
 }
