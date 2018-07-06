3ff69ce9fa5c72771a2897bda2d86b4dec1b3b93
Andrei Ermoshin <iermoshin@gmail.com>
2018-07-06 15:59:01 +0300

    Cache - LFU (test fail)

--- Cache/LFU Cache/LFU Cache/LFUCache.hpp
+++ Cache/LFU Cache/LFU Cache/LFUCache.hpp
@@ -12,6 +12,7 @@
 #include <unordered_map>
 #include <list>
 #include <vector>
+#include <iostream>
 
 class LFUCache {
 private:
@@ -20,41 +21,36 @@ private:
     typedef std::pair<KeyType, ValueType> ListPair;
     typedef std::pair<unsigned char, std::list<ListPair>::iterator> HashMapPair;
     std::unordered_map<KeyType, HashMapPair> hashMap;
-    std::vector<std::list<ListPair>> frequencies;
+    std::list<ListPair> first;
+    std::list<ListPair> second;
     int maxSize;
-    const unsigned char listAmount = 4;
     
     void moveToAnotherFrequency(unsigned char index,
-                                std::list<ListPair> list,
                                 std::list<ListPair>::iterator iterator) {
         // need to move to next list to the end of it
-        if (index < listAmount - 1) {
-            auto nextList = frequencies[index + 1];
+        if (index == 0) {
             // Need to move element from the top of
             // one list to the bottom of another list
-            nextList.splice(nextList.end(), list, iterator);
+            second.splice(second.end(), index == 0 ? first : second, iterator);
             // https://stackoverflow.com/a/4912492
         }
         // else we can't move element, it is on the top already
     }
     
-    void moveInSameFrequency(std::list<ListPair> list,
+    void moveInSameFrequency(unsigned char index,
                              std::list<ListPair>::iterator iterator) {
         // if it's not on the top of the list
         // then need to move element just one position ahead
         auto nextIterator = ++iterator;
         // single element (2) splice method should have 3 arguments
         // http://www.cplusplus.com/reference/list/list/splice/
-        list.splice(nextIterator, list, iterator);
+        (index == 0 ? first : second).splice(nextIterator,
+                                             index == 0 ? first : second,
+                                             iterator);
     }
     
 public:
-    LFUCache(int capacity): maxSize(capacity) {
-        for (int i = 0; i < listAmount; i++) {
-            std::list<ListPair> list;
-            frequencies.insert(frequencies.end(), list);
-        }
-    }
+    LFUCache(int capacity): maxSize(capacity) { }
     
     int get(int key) {
         auto node = hashMap.find(key);
@@ -63,12 +59,14 @@ public:
         }
         
         unsigned char index = node->second.first;
-        auto list = frequencies[index];
         auto iterator = node->second.second;
-        if (iterator == list.begin()) {
-            moveToAnotherFrequency(index, list, iterator);
+        if (index == 0 && iterator == first.begin()) {
+            moveToAnotherFrequency(index, iterator);
+            node->second.first++;
+            node->second.second = --second.end();
         } else {
-            moveInSameFrequency(list, iterator);
+            moveInSameFrequency(index, iterator);
+            node->second.second = --iterator;
         }
         
         int value = node->second.second->second;
@@ -82,11 +80,11 @@ public:
             node->second.second->second = value;
             unsigned char index = node->second.first;
             auto iterator = node->second.second;
-            auto list = frequencies[index];
-            if (iterator == list.begin()) {
-                moveToAnotherFrequency(index, list, iterator);
+            if (iterator == (index == 0 ? first : second).begin()) {
+                moveToAnotherFrequency(index, iterator);
+                node->second.first++;
             } else {
-                moveInSameFrequency(list, iterator);
+                moveInSameFrequency(index, iterator);
             }
             return;
         }
@@ -94,33 +92,28 @@ public:
         if (hashMap.size() == maxSize) {
             // need to remove last value from the last
             // non empty list
-            for (int i = 0; i < listAmount; i++) {
-                auto list = frequencies[i];
-                if (list.size() != 0) {
-                    auto lastNode = list.back();
-                    hashMap.erase(lastNode.first);
-                    list.pop_back();
-                    break;
-                }
+            size_t fsize = first.size();
+            size_t ssize = second.size();
+            if (fsize != 0) {
+                auto lastNode = first.back();
+                hashMap.erase(lastNode.first);
+                first.pop_back();
+            } else if (ssize != 0) {
+                auto lastNode = second.back();
+                hashMap.erase(lastNode.first);
+                second.pop_back();
             }
         }
         
         // Now need to insert completely new value to cache
         ListPair pair(key, value);
-        auto firstList = frequencies.front();
-        firstList.push_back(pair);
-        auto insertedIterator = firstList.end()--;
+        first.push_back(pair);
+        
+        auto insertedIterator = --first.end();
         HashMapPair subPair(0, insertedIterator);
         std::pair<KeyType, HashMapPair> cachePair(key, subPair);
         hashMap.insert(cachePair);
     }
 };
-
-/**
- * Your LFUCache object will be instantiated and called as such:
- * LFUCache obj = new LFUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
 
 #endif /* LFUCache_hpp */
