b508c6b0a49ae97ab0ceff62c89135c29bb86296
Andrei Ermoshin <iermoshin@gmail.com>
2020-05-13 18:07:07 +0300

    Search - Single element in a sorted array (pairs)

---
+++ Search/Single Element in a Sorted Array/Single Element in a Sorted Array/main.swift
@@ -0,0 +1,85 @@
+//
+//  main.swift
+//  Single Element in a Sorted Array
+//
+//  Created by Andrei Ermoshin on 5/13/20.
+//  Copyright © 2020 Andrei Ermoshin. All rights reserved.
+//
+
+import Foundation
+
+class Solution {
+    func singleNonDuplicate(_ nums: [Int]) -> Int {
+        let length = nums.count
+        switch length {
+        case 0:
+            return -1
+        case 1:
+            // not valid case
+            return nums[0]
+        case 2:
+            // not valid case, it is guarantied to be odd count
+            return nums[0]
+        default:
+            // guaranty that at least 3 elements are in array
+            // to check neighbour values
+            break
+        }
+        var r = length - 1
+        var l = 0
+        var m = (r - l)/2
+        var mValue = -1
+        var ml = -1
+        var mr = -1
+        var elementsCount = -1
+        while l < r && m != 0 && m != length {
+            mValue = nums[m]
+            ml = nums[m-1]
+            mr = nums[m+1]
+            // can't be equal to both according to task
+            if ml == mValue && l < m {
+                // checking left side: from l till m - 1 indexes
+                // if left side contains one repeating element for sure
+                // it means that for correctness it should also
+                // contains k pairs of other elements
+                // assert false: should be k + 1 elements `odd` amount of elements
+                // assert true: if it's `even` - it means that number from left side of m index
+                elementsCount = m - l
+                if elementsCount % 2 != 0 {
+                        // expected value, it means that searching element
+                        // from right
+                        l = m + 1
+                    } else {
+                        // not expected, means that element from left
+                        r = m - 1
+                    }
+            } else if mValue == mr && m < r {
+                // should be k + 1 element for correct case without searching number
+                // assert false: k + 1 - odd
+                // assert true: even
+                elementsCount = r - m
+                if elementsCount % 2 != 0 {
+                    // wrong branch, element should be from the left
+                    r = m - 1
+                } else {
+                    l = m + 1
+                }
+            } else {
+                if ml == mValue {
+                    l = m + 1
+                } else if mValue == mr {
+                    r = m - 1
+                } else {
+                    // mValue != ml && mvalue != mr
+                    return mValue
+                }
+            }
+            m = l + (r - l)/2
+        }
+        return nums[m]
+    }
+}
+
+let s = Solution()
+let out1 = s.singleNonDuplicate([1,2,2,3,3])
+print("answer \(out1)")
